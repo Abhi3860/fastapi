2fa32428c054e21902b2fa2e0ca6abaf512e7f90
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-01-31 10:52:58 +0530

    newstuff

--- 1.py
+++ 1.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
-def get_user():
-    return {"message": "Hello World"}
+def get_user(): #normal python function
+    return {"message": "Hello World"} #this gets sent back to the user, also this a dictionary
+#the dictionary is converted to json
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "successfully created post"}
