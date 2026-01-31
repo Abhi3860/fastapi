2b4a737213536f13e9c462152496642509a88fa0
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-01-31 21:33:08 +0530

    CRD

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -24,11 +24,47 @@ my_posts = [{"title":"example post","content":"example content","id":1},{"title"
 def get_posts():
     return {"data":my_posts}
 
+def find_post(id):
+    for p in my_posts:
+        if p['id']==id:
+            return p
+        
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id']= randrange(0,10000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+@app.get("/posts/{id}") #{id} is called a path parameter
+def get_post(id: int, response: Response):
+    post = find_post(int(id))
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {"message":f"Post with id {id} was not found"}
+    
+    return {
+        "post detail": post
+    }
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #deleting a function
+    #find the index of the array that has the required ID
+    #my_posts.pop(index)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+
