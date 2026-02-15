6e6a82d8f9ad2de6e9a289911dea363acf805c18
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-02-15 16:26:37 +0530

    create_post

--- app/1.py
+++ app/1.py
@@ -35,7 +35,9 @@ my_posts = [{"title":"example post","content":"example content","id":1},{"title"
 
 @app.get("/posts")
 def get_posts():
-    return {"data":my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data":posts}
 
 def find_post(id):
     for p in my_posts:
@@ -50,10 +52,10 @@ def find_index_post(id):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.model_dump()
-    post_dict['id']= randrange(0,10000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (name, content, published) VALUES (%s,%s,%s) RETURNING *""", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 @app.get("/posts/{id}") #{id} is called a path parameter
 def get_post(id: int, response: Response):
