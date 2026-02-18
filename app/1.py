0a582a377f4753fc467ff25b51157c8756b4dcc0
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-02-18 15:57:02 +0530

    schemas

--- app/1.py
+++ app/1.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from random import randrange
 import psycopg
 from psycopg.rows import dict_row
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 from sqlmodel import Session
 from sqlmodel import SQLModel
@@ -24,19 +24,6 @@ def get_user(): #normal python function
     return {"message": "Hello World"} #this gets sent back to the user, also this a dictionary
 #the dictionary is converted to json
 
-@app.get("/alchemy")
-def test_post(db: Session = Depends(get_db)):
-
-    posts = db.exec(select(models.Post)).all()
-    return{"data": posts}
-
-
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True #defaults to 'true' if value is not provided
 
 while True:
     try:
@@ -72,7 +59,7 @@ def find_index_post(id):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     #cursor.execute("""INSERT INTO posts (name, content, published) VALUES (%s,%s,%s) RETURNING *""", (post.title, post.content, post.published))
     #new_post = cursor.fetchone()
     #conn.commit()
@@ -118,7 +105,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def create_post(id:int, post: Post, db: Session = Depends(get_db)):
+def create_post(id:int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     #cursor.execute("""UPDATE posts SET name = %s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     #updated_post = cursor.fetchone()
     #conn.commit()
