a787b345a78989bc9d52a125567ec37027a857c4
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-02-20 11:08:43 +0530

    adjustments

--- app/routers/post.py
+++ app/routers/post.py
@@ -24,7 +24,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id:int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
     #cursor.execute("""INSERT INTO posts (name, content, published) VALUES (%s,%s,%s) RETURNING *""", (post.title, post.content, post.published))
     #new_post = cursor.fetchone()
     #conn.commit()
@@ -37,11 +37,11 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
     return new_post
 
 @router.get("/{id}", response_model=schemas.Post) #{id} is called a path parameter
-def get_post(id: int, response: Response,db: Session = Depends(get_db)):
+def get_post(id: int, response: Response,db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
     #cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id),))
     #post = cursor.fetchone()
     post = db.exec(select(models.Post).where(models.Post.id == id)).first()
-    
+    #print(current_user.email)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
         #response.status_code = status.HTTP_404_NOT_FOUND
@@ -50,7 +50,7 @@ def get_post(id: int, response: Response,db: Session = Depends(get_db)):
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id:int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
     
     #cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",(str(id),))
     #deleted_post = cursor.fetchone()
@@ -68,7 +68,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id:int = Depends(oa
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
-def create_post(id:int, post: schemas.PostCreate, db: Session = Depends(get_db), user_id:int = Depends(oauth2.get_current_user)):
+def create_post(id:int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
     #cursor.execute("""UPDATE posts SET name = %s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     #updated_post = cursor.fetchone()
     #conn.commit()
