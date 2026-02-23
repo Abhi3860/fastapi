c13881e1715d54c8a568038f10682de462c3dfb7
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-02-23 13:37:15 +0530

    environment_variables

--- app/1.py
+++ app/1.py
@@ -3,16 +3,7 @@ from .database import engine, get_db
 from sqlmodel import SQLModel
 from sqlmodel import select
 from .routers import post,user, auth
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    database_password: str = "localhost"
-    database_username: str = "postgres"
-    secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-
-settings = Settings()
-print(settings.database_password)
-
+from .config import settings
 
 SQLModel.metadata.create_all(engine)
 app = FastAPI()
--- app/database.py
+++ app/database.py
@@ -2,10 +2,10 @@ from sqlmodel import create_engine, Session
 import psycopg
 from psycopg.rows import dict_row
 import time
+from .config import settings
 
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Hellrider3860@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -14,13 +14,13 @@ def get_db():
     with Session(engine) as session:
         yield session
 
-while True:
-    try:
-        conn = psycopg.connect(host='localhost', dbname='fastapi', user='postgres', password='Hellrider3860', row_factory=dict_row)   
-        cursor =conn.cursor()
-        print("database connection successful")
-        break
-    except Exception as error:
-        print("database connection failed")
-        print("Error:", error)
-        time.sleep(2)
+#while True:
+    # try:
+    #    conn = psycopg.connect(host='localhost', dbname='fastapi', user='postgres', password='Hellrider3860', row_factory=dict_row)   
+    #    cursor =conn.cursor()
+    #   print("database connection successful")
+    #   break
+    #except Exception as error:
+    #    print("database connection failed")
+    #    print("Error:", error)
+    #    time.sleep(2)
