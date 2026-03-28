ca4f3f5d111bdba08866bb6f0fd1d577f3ab7cef
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-03-28 16:10:28 +0530

    cors

--- app/1.py
+++ app/1.py
@@ -4,9 +4,20 @@ from sqlmodel import SQLModel
 from sqlmodel import select
 from .routers import post,user, auth,vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 #SQLModel.metadata.create_all(engine)
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
