db83e2fd2d246f1c5238d17ec2f080998361132d
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-04-08 09:56:45 +0530

    fixing hardcode

--- app/database.py
+++ app/database.py
@@ -5,8 +5,9 @@ import time
 from .config import settings
 import os
 
+
 #SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-DATABASE_URL = "postgresql://fastapi_db_ls1z_user:zdM8aVwrCZzxLCcjQ6nJLZEGyGaDfF6U@dpg-d7at0ls50q8c73aro31g-a.singapore-postgres.render.com/fastapi_db_ls1z"
+DATABASE_URL = settings.database_url
 engine = create_engine(DATABASE_URL)
 
 
